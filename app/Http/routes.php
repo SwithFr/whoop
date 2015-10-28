266150c8036167f7bfc785d9914bbc67faad83f2
SwithFr <contact@swith.fr>
2015-10-28 15:07:04 +0100

    add auth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,9 @@ Route::get( "/home", "PagesController@home" );
 Route::get( "/", "PagesController@home" );
 
 Route::resource('articles', 'ArticlesController');
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
