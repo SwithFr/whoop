b51a2b73cd7c26f3895906ee10bd0ffc5ada609e
SwithFr <contact@swith.fr>
2015-10-28 13:33:01 +0100

    bootstrap templating

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,23 +3,29 @@
 @section( "content" )
 	<h1>liste des articles</h1>
 
+	<div class="row">
+		<p>
+			<a class="btn btn-default" href="{!! route('articles.create') !!}">Créer un article</a>
+		</p>
+	</div>
 	@forelse($articles as $a )
-		<article>
+		<article class="row">
+			<div class="col-lg-9">
 				<h2><a href="{!! route('articles.show', ['id'=>$a->id]) !!}">{{ $a->title  }}</a></h2>
-				<div>{{ $a->body }}</div>
+                <div class="time">
+                    <p>Mis en ligne {!! $a->published_at->diffForHumans() !!}</p>
+                </div>
+				<div class="body">{{ $a->body }}</div>
+			</div>
+			<div class="col-lg-3">
+				<a class="btn btn-warning" href="{!! route( 'articles.edit', ['id' => $a->id] ) !!}">Modifier</a>
+				<a class="btn btn-danger" href="{!! route( 'articles.destroy', ['id' => $a->id] ) !!}">Supprimer</a>
+			</div>
 		</article>
-		<div>
-			<a class="btn btn-warning" href="{!! route( 'articles.edit', ['id' => $a->id] ) !!}">Modifier</a>
-		</div>
 	@empty
 		<div class="alert alert-warning">
 			<p>Oups ! Il semblerait qu'il n'ait aucun article lisible pour le moment</p>
 		</div>
 	@endforelse
 
-	<div>
-		<p>
-			<a class="btn btn-default" href="{!! route('articles.create') !!}">Créer un article</a>
-		</p>
-	</div>
 @stop
