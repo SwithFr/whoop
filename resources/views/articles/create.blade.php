80815708dc90ff5ebd56639640fb8565b4eb07a3
SwithFr <contact@swith.fr>
2015-10-28 14:47:24 +0100

    remove user_id field

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -5,7 +5,6 @@
 	<h1>Ajouter un nouvel article</h1>
 
 	{!! Form::open(['action' => 'ArticlesController@store']) !!}
-		{!! Form::hidden('user_id', '1') !!}
 		@include( "articles.forms.article", ['submitButtonText' => 'Ajouter cet article', 'date' => date('Y-m-d')] )
 	{!! Form::close() !!}
 
