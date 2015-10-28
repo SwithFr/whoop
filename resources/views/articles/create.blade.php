786e811dd6d5fd1ee97e4f0adba558310304a739
SwithFr <contact@swith.fr>
2015-10-28 14:34:29 +0100

    add temporary user id field

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -5,6 +5,7 @@
 	<h1>Ajouter un nouvel article</h1>
 
 	{!! Form::open(['action' => 'ArticlesController@store']) !!}
+		{!! Form::hidden('user_id', '1') !!}
 		@include( "articles.forms.article", ['submitButtonText' => 'Ajouter cet article', 'date' => date('Y-m-d')] )
 	{!! Form::close() !!}
 
