4aac6225055f4b75b9a09200a33c84eef71d6c3f
SwithFr <contact@swith.fr>
2015-10-28 16:47:21 +0100

    check if logged to display buttons to edit and delete

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,11 +3,6 @@
 @section( "content" )
 	<h1>liste des articles</h1>
 
-	<div class="row">
-		<p>
-			<a class="btn btn-default" href="{!! route('articles.create') !!}">Créer un article</a>
-		</p>
-	</div>
 	@forelse($articles as $a )
 		<article class="row">
 			<div class="col-lg-9">
@@ -20,10 +15,12 @@
                 </div>
 				<div class="body">{{ $a->body }}</div>
 			</div>
-			<div class="col-lg-3">
-				<a class="btn btn-warning" href="{!! route( 'articles.edit', ['id' => $a->id] ) !!}">Modifier</a>
-				<a class="btn btn-danger" href="{!! route( 'articles.destroy', ['id' => $a->id] ) !!}">Supprimer</a>
-			</div>
+			@if(Auth::check())
+				<div class="col-lg-3">
+					<a class="btn btn-warning" href="{!! route( 'articles.edit', ['id' => $a->id] ) !!}">Modifier</a>
+					<a class="btn btn-danger" href="{!! route( 'articles.destroy', ['id' => $a->id] ) !!}">Supprimer</a>
+				</div>
+			@endif
 		</article>
 	@empty
 		<div class="alert alert-warning">
