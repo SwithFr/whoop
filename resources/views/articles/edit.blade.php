3ef7cf9aa9353ebb707febd63de0e48e2fa95686
SwithFr <contact@swith.fr>
2015-10-28 11:21:50 +0100

    complete edit form

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -4,6 +4,9 @@
 
 	<h1>Editer un article</h1>
 
-
+	{!! Form::model($article, ['action' => ['ArticlesController@update',$article->id], 'class' => 'form-horizontal']) !!}
+	{!! method_field( "PATCH" ) !!}
+       @include( "articles.forms.article", ['submitButtonText' => 'Mettre à jour'] )
+    {!! Form::close() !!}
 
 @stop
