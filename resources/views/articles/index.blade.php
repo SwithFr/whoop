072b49e3f51c35d333f9f6500c818fa1ab875680
SwithFr <contact@swith.fr>
2015-10-28 11:53:46 +0100

    add edit btn

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,6 +8,9 @@
 				<h2><a href="{!! route('articles.show', ['id'=>$a->id]) !!}">{{ $a->title  }}</a></h2>
 				<div>{{ $a->body }}</div>
 		</article>
+		<div>
+			<a class="btn btn-warning" href="{!! route( 'articles.edit', ['id' => $a->id] ) !!}">Modifier</a>
+		</div>
 	@empty
 		<div class="alert alert-warning">
 			<p>Oups ! Il semblerait qu'il n'ait aucun article lisible pour le moment</p>
