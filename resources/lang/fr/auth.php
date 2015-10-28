4e3e80891442571dfc91a81f44ae116aa966cc27
SwithFr <contact@swith.fr>
2015-10-28 17:09:31 +0100

    add fr local

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Le login ou le mot de passe ne correspondent pas',
+    'throttle' => 'Trop de tentatives successives on échouées, vueillez attendre :seconds secondes.',
+
+];
