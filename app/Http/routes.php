ce680fd9200fcf69443fcd9d03be4b3feb6761da
SwithFr <contact@swith.fr>
2015-10-28 10:47:38 +0100

    use resource instead

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,11 +13,6 @@
 
 Route::get( "/about", "PagesController@about" );
 Route::get( "/home", "PagesController@home" );
+Route::get( "/", "PagesController@home" );
 
-Route::get( "/articles", "ArticlesController@index" );
-Route::get( "/article/{id}", "ArticlesController@show" )->name('showArticle')->where( "id", "[0-9]+" );
-Route::get( "/article/create", "ArticlesController@create" );
-Route::post( "/article/create", "ArticlesController@store" );
-Route::get( "/article/{id}/edit", "ArticlesController@edit" )->where( "id", "[0-9]+" );
-// patch update a partial of resource vs put update a complete resource
-Route::patch( "/article/update", "ArticlesController@update" );
+Route::resource('articles', 'ArticlesController');
