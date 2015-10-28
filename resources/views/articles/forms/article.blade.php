35b091606780eed807a0ebb24c6e18f093c42ae5
SwithFr <contact@swith.fr>
2015-10-28 17:28:22 +0100

    add class if has error

--- resources/views/articles/forms/article.blade.php
+++ resources/views/articles/forms/article.blade.php
@@ -1,15 +1,15 @@
-<div class="form-group">
+<div class="form-group {{ $errors->has('title') ? "has-error" : ''  }}">
 	{!! Form::label('title','Titre', ['class' => 'control-label']) !!}
 	{!! Form::text('title', null,['class' => 'form-control']) !!}
 </div>
 
-<div class="form-group">
+<div class="form-group {{ $errors->has('date') ? "has-error" : ''  }}">
 	{!! Form::label('date','Date de publication') !!}
 	{!! Form::input('date', 'published_at', $date, ['class' => 'form-control']) !!}
 </div>
 
-<div class="form-group">
-	{!! Form::label('content', 'Contenu') !!}
+<div class="form-group {{ $errors->has('body') ? "has-error" : ''  }}">
+	{!! Form::label('body', 'Contenu') !!}
 	{!! Form::textarea('body', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
