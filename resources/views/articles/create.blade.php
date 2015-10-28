df98e034cda8304e791d108ef0573001b13e11c9
SwithFr <contact@swith.fr>
2015-10-28 11:52:22 +0100

    pass date to form

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -5,7 +5,7 @@
 	<h1>Ajouter un nouvel article</h1>
 
 	{!! Form::open(['action' => 'ArticlesController@store']) !!}
-		@include( "articles.forms.article", ['submitButtonText' => 'Ajouter cet article'] )
+		@include( "articles.forms.article", ['submitButtonText' => 'Ajouter cet article', 'date' => date('Y-m-d')] )
 	{!! Form::close() !!}
 
 @stop
