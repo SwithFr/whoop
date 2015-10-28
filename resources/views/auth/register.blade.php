524c550dbe8f2fb537dbd0073591f378e379f25b
SwithFr <contact@swith.fr>
2015-10-28 17:03:55 +0100

    fix class name for labels

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,22 +6,22 @@
 	    {!! csrf_field() !!}
 
 	    <div class="form-group {{ $errors->has('name') ? "has-error" : ''  }}">
-	         <label class="label-control" for="name">Name</label>
+	         <label class="control-label" for="name">Name</label>
 	        <input type="text" name="name" value="{{ old('name') }}">
 	    </div>
 
 	    <div class="form-group {{ $errors->has('email') ? "has-error" : ''  }}">
-	        <label class="label-control" for="email">Email</label>
+	        <label class="control-label" for="email">Email</label>
 	        <input type="email" name="email" value="{{ old('email') }}">
 	    </div>
 
 	    <div class="form-group {{ $errors->has('password') ? "has-error" : ''  }}">
-	        <label class="label-control" for="password">Password</label>
+	        <label class="control-label" for="password">Password</label>
 	        <input type="password" name="password">
 	    </div>
 
 	    <div class="form-group {{ $errors->has('password_confrimation') ? "has-error" : ''  }}">
-	        <label class="label-control" for="password_confirmation">Confirm Password</label>
+	        <label class="control-label" for="password_confirmation">Confirm Password</label>
 	        <input type="password" name="password_confirmation">
 	    </div>
 
