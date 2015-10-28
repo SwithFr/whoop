881df67944c704cd2bf087978fc5660ef0dca411
SwithFr <contact@swith.fr>
2015-10-28 16:59:22 +0100

    add error login and register template

---
+++ resources/views/errors/loginErrors.blade.php
@@ -0,0 +1,9 @@
+@if(count($errors))
+	<div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $e)
+                <li>{!! $e !!}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
