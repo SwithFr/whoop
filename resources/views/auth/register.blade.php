120abf4ff6c8cd46be12b69bc61793e682cac91c
SwithFr <contact@swith.fr>
2015-10-28 17:00:21 +0100

    add class if has error

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,25 +1,26 @@
 @extends( "layout" )
 
 @section( "content" )
+	@include( "errors.loginErrors" )
 	<form method="POST" class="form-horizontal" action="/auth/register">
 	    {!! csrf_field() !!}
 
-	    <div class="form-group">
+	    <div class="form-group {{ $errors->has('name') ? "has-error" : ''  }}">
 	         <label class="label-control" for="name">Name</label>
 	        <input type="text" name="name" value="{{ old('name') }}">
 	    </div>
 
-	    <div class="form-group">
+	    <div class="form-group {{ $errors->has('email') ? "has-error" : ''  }}">
 	        <label class="label-control" for="email">Email</label>
 	        <input type="email" name="email" value="{{ old('email') }}">
 	    </div>
 
-	    <div class="form-group">
+	    <div class="form-group {{ $errors->has('password') ? "has-error" : ''  }}">
 	        <label class="label-control" for="password">Password</label>
 	        <input type="password" name="password">
 	    </div>
 
-	    <div class="form-group">
+	    <div class="form-group {{ $errors->has('password_confrimation') ? "has-error" : ''  }}">
 	        <label class="label-control" for="password_confirmation">Confirm Password</label>
 	        <input type="password" name="password_confirmation">
 	    </div>
