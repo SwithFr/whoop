523ca96fc9fdc274230a5ebb8a9b8d868ee7eb45
SwithFr <contact@swith.fr>
2015-10-29 09:57:40 +0100

    change id to slug

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -6,7 +6,7 @@
 	@forelse($articles as $a )
 		<article class="row">
 			<div class="col-lg-9">
-				<h2><a href="{!! route('articles.show', ['id'=>$a->id]) !!}">{{ $a->title  }}</a></h2>
+				<h2><a href="{!! route('articles.show', ['slug'=>$a->slug]) !!}">{{ $a->title  }}</a></h2>
                 <div class="time">
                     <p class="text-muted">Mis en ligne par <span class="text-primary">{{ $a->user->name }}</span> {!! $a->published_at->diffForHumans() !!}</p>
                 </div>
@@ -14,8 +14,8 @@
 			</div>
 			@if(Auth::check())
 				<div class="col-lg-3">
-					<a class="label label-warning" href="{!! route( 'articles.edit', ['id' => $a->id] ) !!}">Modifier</a>
-					<a class="label label-danger" href="{!! route( 'articles.destroy', ['id' => $a->id] ) !!}">Supprimer</a>
+					<a class="label label-warning" href="{!! route( 'articles.edit', ['slug' => $a->slug] ) !!}">Modifier</a>
+					<a class="label label-danger" href="{!! route( 'articles.destroy', ['slug' => $a->slug] ) !!}">Supprimer</a>
 				</div>
 			@endif
 		</article>
